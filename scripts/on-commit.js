7fbfa4b65863588bfbd2aa75c7275c4be25961ee
adokaeva@issart.com <adokaeva@issart.com>
2018-10-16 18:09:35 +0600

    GIT-3007:Code commit on issue -> check status and transition script was implemented

---
+++ scripts/on-commit.js
@@ -0,0 +1,62 @@
+//# sourceURL=on-commit.js
+'use strict';
+
+//  Classes passed as parameters by default
+//
+//  The commit revision ID.
+// revision : java.lang.String
+//
+//  The full commit message.
+// commitComment : java.lang.String
+//
+//  The commit timestamp (unixtime format).
+// commitTime : long
+//
+//  An e-mail of the commit author.
+// authorEmail : java.lang.String
+//
+//  An author name of the commit.
+// authorName : java.lang.String
+//
+//  A name of the branch where the commit belongs to.
+//  Please note that commit is processed only once and branchName is a first branch where the commit is found.
+// branchName : java.lang.String
+//
+//  An object with several commit properties and flags.
+// commitProperties : com.bigbrassband.jira.git.services.indexer.revisions.CommitProcessor.CommitProperties
+//   issueCommit         -- true if the commit has issue keys in its comment message.
+//   newForRepoAndCommit -- true if the commit is seen for the first time for this repository. It seems it is always true.
+//   initialIndex        -- true if the script is called during an initial repository reindex. It seems it is always false.
+
+load(__DIR__ + 'utils.js');
+
+var IN_PROGRESS = "Start Progress";
+var OPEN = "Open";
+
+var workflowManager = getComponent("com.atlassian.jira.workflow.WorkflowManager");
+var issueManager = getComponent("com.atlassian.jira.issue.IssueManager");
+var userService = getComponent("com.bigbrassband.jira.git.services.users.GitJiraUsersUtil");
+
+var issueKey = getIssueKey(branchName);
+if(issueKey !== null) {
+    var issue = issueManager.getIssueByCurrentKey(issueKey);
+    var status = issue.getStatus();
+    var user = userService.getUserByEmail(authorEmail);
+    print(user);
+    if(status.getName() === OPEN) {
+        var possibleActionsList = getAcceptedNextSteps(workflowManager, issue);
+        var newStatusId = getIdForStatusWithName(IN_PROGRESS, possibleActionsList);
+        var issueService = getComponent("com.atlassian.jira.bc.issue.IssueService");
+        if(newStatusId) {
+            var issueService = getComponent("com.atlassian.jira.bc.issue.IssueService");
+            var transitionValidationResult = issueService.validateTransition(
+                user, issue.getId(), newStatusId, issueService.newIssueInputParameters()
+            );
+            if (transitionValidationResult.isValid()) {
+                var transitionResult =  issueService.transition(user, transitionValidationResult);
+            } else {
+                print(transitionValidationResult.getErrorCollection());
+            }
+        }
+    }
+}
