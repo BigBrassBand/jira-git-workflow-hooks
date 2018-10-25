fca66b5d2de889c1c09ee05eb48996addd8862cc
adokaeva@issart.com <adokaeva@issart.com>
2018-10-25 15:32:17 +0600

    GIT-3010:corrections after review, changes in code style

--- scripts/on-user-resolution.js
+++ scripts/on-user-resolution.js
@@ -1,6 +1,16 @@
 //# sourceURL=on-user-resolution.js
 'use strict';
 
+//  Description
+//
+// This script work as a handler that force all commits to be processed by a Jira user called smartcommituser.
+// If smartcommituser is unavailable, then return an error.
+//
+// https://github.com/BigBrassBand/jira-git-workflow-hooks
+//
+//  Applying
+// Set name of smart commit user in constant SMARTCOMMIT_USER_NAME. By default "smartcommituser" is used.
+//
 //  Classes passed as parameters by default
 //
 //  The commit revision ID.
@@ -25,28 +35,26 @@
 //    setResultCode(com.bigbrassband.jira.git.services.scripting.UserResolutionResult.ResultCode resultCode) -- USER_FOUND, USE_STANDARD_SEARCH, ERROR
 //    setErrorMessage(java.lang.String errorMessage)
 
-function getComponent(componentName) {
-	var componentAccessor = Java.type("com.atlassian.jira.component.ComponentAccessor");
-	var clazz = Java.type("java.lang.Class")
-	return componentAccessor.getOSGiComponentInstanceOfType(clazz.forName(componentName));
-}
-
-// this user has to have admin rights
+//set name of smart commits' user
 var SMARTCOMMIT_USER_NAME = "smartcommituser";
 
+//get needed components to execute main logic
 var issueService = getComponent("com.atlassian.jira.bc.issue.IssueService");
 var crowdService = getComponent("com.atlassian.jira.plugins.dvcs.smartcommits.GitPluginCompatibilityCrowdService");
 var resultCode = Java.type("com.bigbrassband.jira.git.services.scripting.UserResolutionResult.ResultCode");
 
+//find user by user name
 var users = crowdService.getUsersByName(SMARTCOMMIT_USER_NAME);
-
+//user was not found
 if (users.isEmpty()) {
-	resolutionResult.setErrorMessage("Unknown Jira user:" + SMARTCOMMIT_USER_NAME);
-	resolutionResult.setResultCode(resultCode.ERROR);
+    resolutionResult.setErrorMessage("Unknown Jira user:" + SMARTCOMMIT_USER_NAME);
+    resolutionResult.setResultCode(resultCode.ERROR);
 } else if (users.size() > 1) {
-  	resolutionResult.setErrorMessage("Ambiguous jira user");
-  	resolutionResult.setResultCode(resultCode.ERROR);
+    //several users were found
+    resolutionResult.setErrorMessage("Ambiguous jira user");
+    resolutionResult.setResultCode(resultCode.ERROR);
 } else {
-  	resolutionResult.setUser(users.get(0));
-  	resolutionResult.setResultCode(resultCode.USER_FOUND);
+    //single user was found
+    resolutionResult.setUser(users.get(0));
+    resolutionResult.setResultCode(resultCode.USER_FOUND);
 }
