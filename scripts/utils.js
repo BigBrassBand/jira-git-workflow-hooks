f49401c0215eb758524f28b9a0f5acaa582fb498
Denis Demidov <ddemidov@issart.com>
2021-06-11 19:15:36 +0700

    GIT-4404 Added log messages.

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,71 +1,92 @@
-//# sourceURL=utils.js
+//# sourceURL=on-branch-created.js
 'use strict';
 
-//get first issue key from stringField
-function getIssueKey(stringField) {
-    var regexp = /([A-Za-z][A-Za-z0-9_]{1,19}-\d+)/g;
-    var keys = [];
-    var result = regexp.exec(stringField);
-    while (result) {
-        keys.push(result[0]);
-        result = regexp.exec(stringField);
-    }
-    return keys.length > 0 ? keys[0] : null;
-}
+//  Description
+//
+// This script does an automatic issue transition to "In progress" status if
+// the issue is still in "Open" status when a branch has been created for the issue.
+// It works on the assumption that an issue key is present in the name of the created branch.
+// First issue's key from the created branch name will be taken as an issue for the transition.
+// Issue's status will be changed on behalf of the issue's assignee.
+//
+// https://github.com/BigBrassBand/jira-git-workflow-hooks
+//
+//  Applying
+// Set name of open status in constant OPEN. By default "Open" is used.
+// Set name of needed transition in constant IN_PROGRESS. By default "Start Progress" is used.
+//
+//  Classes passed as parameters by default
+//
+//  A name of the branch.
+// branchName : java.lang.String
+//
+//  The branch commit revision ID
+// revision : java.lang.String
+//
+//  Our internal repository ID. It may be used as an opaque unique ID to distinguish repositories.
+// repositoryId : java.lang.Integer
+//
+//  Display name of th repository.
+// repositoryName : java.lang.String
+//
+// Origin of the repository (http/git URL for remote repositories or just a folder path for tracked repositories). It is empty for hosted repositories.
+// repositoryOrigin : java.lang.String
+(function () {
+    load(__DIR__ + 'utils.js');
 
-//get accepted transitions from issue
-function getAcceptedNextSteps(workflowManager, issue) {
+    //set required statuses names
+    var IN_PROGRESS = "Start Progress";
+    var OPEN = "Open";
+
+    //get needed components to execute main logic
+    var workflowManager = getComponent("com.atlassian.jira.workflow.WorkflowManager");
+    var issueManager = getComponent("com.atlassian.jira.issue.IssueManager");
+
+    //get first issue key present in branchName
+    var issueKey = getIssueKey(branchName);
+    //check that issueKey is presenr in branch name
+    if (issueKey == null) {
+        getJiraLogger().debug("Can't find any issue key in the given branch name '" + branchName + "'. Exiting...");
+        return;
+    }
+
+    //find issue by key
+    getJiraLogger().debug("Looking for the issue with key '" + issueKey + "'...");
+    var issue = issueManager.getIssueByCurrentKey(issueKey);
     var status = issue.getStatus();
-    var workFlow = workflowManager.getWorkflow(issue);
-    var currentStep = workFlow.getLinkedStep(status);
-    return currentStep.getActions();
-}
-
-//find status id by statusName among possibleActionsList
-function getIdForStatusWithName(statusName, possibleActionsList) {
-    var actionId = 0;
-    for each(var actionDescriptor in possibleActionsList) {
-        if (actionDescriptor.getName() === statusName) {
-            return actionDescriptor.getId();
-        }
+    // use issue's assignee as user
+    var user = issue.getAssignee();
+    // check that issue has OPEN status
+    getJiraLogger().debug("The issue with key '" + issueKey + "' has status '" + status.getName() + "'.");
+    if (status.getName() !== OPEN) {
+        getJiraLogger().debug("The issue with key '" + issueKey + "' has undesirable status. Exiting...");
+        return;
     }
-    return null;
-}
 
-//find status id by statusName among possibleActionsList
-function getIdForStatusWithNameIgnoreCase(statusName, possibleActionsList) {
-    var actionId = 0;
-    for each (var actionDescriptor in possibleActionsList) {
-       	if (actionDescriptor.getName().toLowerCase() === statusName) {
-       	    return actionDescriptor.getId();
-       	}
+    var possibleActionsList = getAcceptedNextSteps(workflowManager, issue);
+    //retrieve new status id by his name from possible next statuses
+    var newStatusId = getIdForStatusWithName(IN_PROGRESS, possibleActionsList);
+    //if new status name is correct
+    if (newStatusId == null) {
+        getJiraLogger().debug("Can't determine the next status for the issue with key '" + issueKey + "'. Exiting...");
+        return;
     }
-    return null;
-}
 
-//get component by component full name
-function getComponent(componentName) {
-    var componentAccessor = Java.type("com.atlassian.jira.component.ComponentAccessor");
-    var clazz = Java.type("java.lang.Class")
-    return componentAccessor.getOSGiComponentInstanceOfType(clazz.forName(componentName));
-}
-
-//form error message
-function formError(issue, i18nHelper, commandName, templateName) {
-
-    function getIssueState(issue, i18nHelper) {
-        var s = issue.getStatusObject();
-        return s == null ? i18nHelper.getText(NO_STATUS) : s.getName();
-    };
-
-    var errorHandler = Java.type("com.atlassian.jira.plugins.dvcs.smartcommits.model.CommitHookHandlerError");
-    return errorHandler.fromSingleError(
-        commandName, issue.getKey(), i18nHelper.getText(templateName, issue.getKey(), getIssueState(issue))
+    //get service to work with issue
+    var issueService = getComponent("com.atlassian.jira.bc.issue.IssueService");
+    //validate changes
+    var transitionValidationResult = issueService.validateTransition(
+        user, issue.getId(), newStatusId, issueService.newIssueInputParameters()
     );
-}
-
-// Returns a logger to write into the Jira logs
-function getJiraLogger() {
-    return Java.type("org.apache.log4j.Logger").getLogger("com.bigbrassband.jira.git.services.scripting.ScriptService");
-}
-
+    if (!transitionValidationResult.isValid()) {
+        print("On-branch-created script execution:");
+        print("repositoryName =", repositoryName);
+        print("branchName =", branchName);
+        print("Errors during transition:");
+        print(transitionValidationResult.getErrorCollection());
+        getJiraLogger().debug("Errors during transition for the issue with key '" + issueKey + "': " + transitionValidationResult.getErrorCollection());
+    } else {
+        issueService.transition(user, transitionValidationResult);
+        getJiraLogger().debug("Transition for the issue with key '" + issueKey + "' completed.");
+    }
+})();
